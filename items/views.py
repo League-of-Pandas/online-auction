ce3fe2da4e4376ae3c50b84879af9cc63cb52bb7
abdullahnazzal <a.nazzal1995@gmail.com>
2021-12-12 22:10:21 +0200

    fix: the register

--- items/views.py
+++ items/views.py
@@ -2,17 +2,17 @@ from rest_framework.generics import (
     ListCreateAPIView,
     RetrieveUpdateDestroyAPIView,
 )
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import AllowAny
 from .models import Item
 from .serializers import ItemSerializer
-from .permissions import IsOwnerOrReadOnly
+# from .permissions import IsOwnerOrReadOnly
 
 class ItemsList(ListCreateAPIView):
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    permission_classes = (AllowAny,)
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
 
 class ItemDetail(RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsOwnerOrReadOnly,)
+    permission_classes = (AllowAny,)
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
