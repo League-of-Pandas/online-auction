793fe384cdc914556ae7e4fa31721d608ed818fd
anas-abusaif <anasabusief@gmail.com>
2021-12-13 22:43:28 +0200

    refactor: updated permissions tohandle requests properly

--- items/models.py
+++ items/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.db.models.aggregates import Max
 from django.utils import timezone
 
 # Create your models here.
 
 class Item(models.Model):
-  item_name = models.CharField(max_length=64)
+  item_name = models.CharField(max_length=64 , blank=True, default="")
   image = models.CharField(max_length=10000)
   CATEGORY_CHOICES = [
     ("Vehicles", 'Vehicles'),
@@ -23,11 +22,11 @@ class Item(models.Model):
     )
   
   description = models.TextField()
-  init_price = models.IntegerField()
+  init_price = models.IntegerField(blank=True, null=True)
   highest_bidding = models.IntegerField(default=0)
 
   bid_increment = models.IntegerField(default=0)
-  owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+  owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE ,blank=True, null=True)
   bidder = models.ForeignKey(get_user_model(),related_name="bidder", blank=True, null=True,on_delete=models.DO_NOTHING)
 
   start_date = models.DateTimeField(('start_date'), default=timezone.now)
