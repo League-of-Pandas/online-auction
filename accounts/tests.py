dcfd99decc76c368b8a0f7df322b3baab9c41b1e
tahany777 <tahanymohammed777@hotmail.com>
2021-12-16 06:24:19 +0200

    test: fix tests for accounts

--- accounts/tests.py
+++ accounts/tests.py
@@ -76,63 +76,55 @@ class APIUserTest(APITestCase):
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED, user.id)
 
-    # def test_user_update(self):
-    #     user = get_user_model().objects.create_user(username='tahany',password='0000')
-    #     user.save()
-
-    #     test_item = CustomUser.objects.create(
-    #         is_superuser= True,
-    #         username= "tahani",
-    #         first_name= "tahani",
-    #         last_name= "ali",
-    #         email= "tahani95@admin.com",
-    #         is_staff= True,
-    #         is_active= True,
-    #         date_joined= "2021-12-12T12:04:50.450159Z",
-    #         location= 'location',
-    #         number= '1223447',
-    #     )
-    #     test_item.save()
-
-    #     url = reverse('user_detail',args=[test_item.id])
-    #     data = {
-    #         "is_superuser" : True,
-    #         "username" : "tahani",
-    #         "first_name" : "tahani",
-    #         "last_name": "ali",
-    #         "password": "01234tahany",
-    #         "email" : "tahani95@admin.com",
-    #         "is_staff":True,
-    #         "is_active": True,
-    #         "date_joined": "2021-12-12T12:04:50.450159Z",
-    #         "location":"location",
-    #         "number":"1223447", 
-    #     }
-    #     response = self.client.put(url, data, format='json')
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK, url)
-
-    # def test_delete(self):
-    #         """Test the user can delete a item."""
-
-    #         user = get_user_model().objects.create_user(username='tahany',password='0000')
-    #         user.save()
-
-    #         test_user = CustomUser.objects.create(
-    #             is_superuser= True,
-    #             username= "tahany",
-    #             first_name= "tahany",
-    #             last_name= "ali",
-    #             email= "tahany@admin.com",
-    #             is_staff= True,
-    #             is_active= True,
-    #             date_joined= "2021-12-12T12:04:50.450159Z",
-    #             location= 'location',
-    #             number= '1223447',
-    #         )
-    #         test_user.save()
-    #         item = CustomUser.objects.get()
-    #         url = reverse('user_detail', kwargs={'pk': item.id})
-    #         response = self.client.delete(url)
-    #         print(response.data)
-    #         self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT, url)
+    def test_user_update(self):
+        user = CustomUser.objects.create(
+            is_superuser= True,
+            username= "tester",
+            first_name= "tahani",
+            last_name= "ali",
+            email= "tahani95@admin.com",
+            is_staff= True,
+            is_active= True,
+            date_joined= "2021-12-12T12:04:50.450159Z",
+            location= 'location',
+            number= '1223447',
+        )
+        user.save()
+        url = reverse('user_detail',args=[user.id])
+        data = {
+            "is_superuser" : True,
+            "username" : "tester",
+            "first_name" : "tahani",
+            "last_name": "ali",
+            "password": "whate001",
+            "email" : "tahani95@admin.com",
+            "is_staff":True,
+            "is_active": True,
+            "date_joined": "2021-12-12T12:04:50.450159Z",
+            "location":"location",
+            "number":"1223447",
+        }
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK, url)
+        self.assertEqual(CustomUser.objects.count(), user.id)
+        self.assertEqual(CustomUser.objects.get().username, data['username'])
+    def test_delete(self):
+            """Test the user can delete a item."""
+            test_user = CustomUser.objects.create(
+                is_superuser= True,
+                username= "tahani",
+                first_name= "tahani",
+                last_name= "ali",
+                email= "tahani95@admin.com",
+                is_staff= True,
+                is_active= True,
+                date_joined= "2021-12-12T12:04:50.450159Z",
+                location= 'location',
+                number= '1223447',
+            )
+            test_user.save()
+            item = CustomUser.objects.get()
+            url = reverse('user_detail', kwargs={'pk': item.id})
+            response = self.client.delete(url)
+            self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT, url)
 
